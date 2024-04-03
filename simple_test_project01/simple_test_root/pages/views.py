3e7e1176afe8946320d58be29146f1a080f22663
Mahek Hemani <mahekhemani@MacBook-Pro.local>
2024-04-03 09:50:06 +0100

    week 8

--- simple_test_project01/simple_test_root/pages/views.py
+++ simple_test_project01/simple_test_root/pages/views.py
@@ -1,8 +1,10 @@
 # \simple_test_root\pages\views.py
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail, get_connection
 from . models import Page
 from datetime import date # Add this line near top of file
+from .contact import ContactForm
 
 
 def index(request, pagename=''):
@@ -19,3 +21,29 @@ def index(request, pagename=''):
 	    'welcome': f'Welcome to {month} {year}',
     }
     return render(request, 'pages/page.html', context)
+
+def contact(request):
+	submitted = False
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			con = get_connection('django.core.mail.backends.console.EmailBackend')
+			send_mail(
+				cd['subject'],
+				cd['message'],
+				cd.get('email', 'noreply@dcu.ie'),
+				['student@dcu.ie'], # change this
+				connection=con
+			)
+			return HttpResponseRedirect('/contact?submitted=True')
+	else:
+		form = ContactForm()
+		if 'submitted' in request.GET:
+			submitted = True
+	context = {
+		'form': form,
+		'page_list': Page.objects.all(),
+		'submitted': submitted
+	}
+	return render(request, 'pages/contact.html', context)
