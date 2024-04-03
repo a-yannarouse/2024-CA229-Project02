241e1462ab2075d2d253fc0d8c47a755f143ab73
Mahek Hemani <mahekhemani@MacBook-Pro.local>
2024-04-03 09:17:48 +0100

    week 3

--- simple_test_project01/simple_test_root/pages/views.py
+++ simple_test_project01/simple_test_root/pages/views.py
@@ -1,12 +1,21 @@
-# simple_test_root/pages/views.py
+# \simple_test_root\pages\views.py
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from . models import Page
 
+
 def index(request):
-	pg = Page.objects.get(permalink='/')
-	return HttpResponse(pg.bodytext)
+    pg = Page.objects.get(permalink='/')
+    context = {
+	'title': pg.title,
+	'content': pg.bodytext
+    }
+    return render(request, 'base.html', context)
 
 def about(request):
-	pg = Page.objects.get(permalink='/kpop')
-	return HttpResponse(pg.bodytext)
+    pg = Page.objects.get(permalink='/kpop')
+    context = {
+	'title': pg.title,
+	'content': pg.bodytext
+    }
+    return render(request, 'kpop.html', context)
