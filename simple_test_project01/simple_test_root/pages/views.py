3e3131d0614671762278b3d3449addc502ae0979
Mahek Hemani <mahekhemani@MacBook-Pro.local>
2024-04-03 09:43:20 +0100

    week 5

--- simple_test_project01/simple_test_root/pages/views.py
+++ simple_test_project01/simple_test_root/pages/views.py
@@ -2,15 +2,20 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from . models import Page
+from datetime import date # Add this line near top of file
 
 
 def index(request, pagename=''):
     pagename = '/' + pagename
     pg = Page.objects.get(permalink=pagename)
+    t = date.today()
+    month = t.strftime('%b')
+    year = t.year
     context = {
     	'title': pg.title,
-	    'content': pg.bodytext,
+    	'content': pg.bodytext,
         'last_updated': pg.update_date,
 		'page_list': Page.objects.all(),
+	    'welcome': f'Welcome to {month} {year}',
     }
     return render(request, 'pages/page.html', context)
